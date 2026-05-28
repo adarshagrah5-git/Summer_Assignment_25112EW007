78cb757a3dbbeb895edcf69d46d7fd190d0f40fd
adarshagrah5-git <adarshagrah5@gmail.com>
2026-05-28 21:17:58 +0530

    Add files via upload
    This is the start of summer vacation.

---
+++ daywise_Assignment/day_1/q2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int n,table;
+    scanf("%d",&n);
+    for (int i=1;i<=10;i++){
+        table=n*i;
+        printf("%d x %d = %d\n", n, i, table);
+    }
+    return 0;
+}
